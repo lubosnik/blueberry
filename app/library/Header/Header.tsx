93ff7150e0b5bc6ffa3be096f0a2e7e73517b6c7
Lubos <lubos.kuchar@sk.ibm.com>
2023-12-03 19:11:33 +0100

    Image loader test

--- app/library/Header/Header.tsx
+++ app/library/Header/Header.tsx
@@ -1,15 +1,23 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import bgImage from '../../../public/bgImage.png';
 import { Blueberry } from './assets/Blueberry';
 import { Bow } from '../Bow';
 
 export const Header = forwardRef<HTMLDivElement>((_, ref) => {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <header
       ref={ref}
       className="static flex items-center justify-center max-h-[70vh]  max-w-full overflow-hidden"
     >
-      <img className=" w-full " src={bgImage.src} />
+      {!isLoaded && <div> Loading</div>}
+      {isLoaded && (
+        <img
+          className=" w-full "
+          src={bgImage.src}
+          onLoad={() => setIsLoaded(true)}
+        />
+      )}
       <header className="absolute flex flex-col items-center">
         <h1 className="mx-auto text-center text-2xl font-semibold tracking-widest">
           Folklórny súbor
